9d9be14d3ff3965de81f1a42cdfd4994837c9687
Casey Barkan <caseybarkan@Caseys-MBP-3.lan>
2024-05-20 23:58:36 -0700

    billiard_funcs now includes propogate function

--- Fig1ABC.py
+++ Fig1ABC.py
@@ -5,7 +5,7 @@ Generates Figure 1 panels A, B, and C
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-from billiard_funcs import trajectory_collisions
+from billiard_funcs import Triangle, trajectory_collisions
 from plot_with_arrow import plot_with_arrow
 
 # Specifying the shape of the triangle
@@ -17,6 +17,7 @@ from plot_with_arrow import plot_with_arrow
 L1r = 0.9
 L1l = -.3
 L2 = 1.
+triangle = Triangle(L1l,L1r,L2)
 
 # Initializing figure, subplots a,b,c correspond to panels A, B, C
 fig, (a,b,c) = plt.subplots(1,3,figsize=(10.5,3.5))
@@ -32,7 +33,7 @@ vy0 = -0.4 # Initial velocity vy
 t = 5.8 # Time for trajectory
 
 # Find coordinates of collisions with walls, ts=collision times
-xs,ys,ts = trajectory_collisions(x0,y0,vx0,vy0,t,L1l,L1r,L2)
+xs,ys,ts = trajectory_collisions(x0,y0,vx0,vy0,t,triangle)
 plot_with_arrow(a,xs,ys,linestyle='--',length=0.08)
 
 ###
@@ -53,32 +54,32 @@ c.plot([L1l/3,.3],[2*L2/3,2*L2/3],':k')
 
 # Plot axes and triangle for each subplot.
 for ax in [a,b,c]:
-	
-	# Turn off default axes:
-	ax.axis('off')
+    
+    # Turn off default axes:
+    ax.axis('off')
 
-	# Plot customized axes:
-	ax_color='grey' #color of axis lines
-	axis_extension = 0.2 #how far axes extend past triangle
-	plot_with_arrow(ax,[0,L1r+axis_extension],[0,0],color=ax_color,
+    # Plot customized axes:
+    ax_color='grey' #color of axis lines
+    axis_extension = 0.2 #how far axes extend past triangle
+    plot_with_arrow(ax,[0,L1r+axis_extension],[0,0],color=ax_color,
                     arrow_style='V',tail_style=None)
-	plot_with_arrow(ax,[0,L1l-axis_extension],[0,0],color=ax_color,
+    plot_with_arrow(ax,[0,L1l-axis_extension],[0,0],color=ax_color,
                     arrow_style='V',tail_style=None)
-	plot_with_arrow(ax,[0,0],[0,L2+axis_extension],color=ax_color,
+    plot_with_arrow(ax,[0,0],[0,L2+axis_extension],color=ax_color,
                     arrow_style='V',tail_style=None)
-	plot_with_arrow(ax,[0,0],[0,-axis_extension],color=ax_color,
+    plot_with_arrow(ax,[0,0],[0,-axis_extension],color=ax_color,
                     arrow_style='V',tail_style=None)
 
-	#Plot triangle
-	linewidth=3
+    #Plot triangle
+    linewidth=3
     """ Sides of the triangle are plotted with solid_capstyle='round' to create 
     rounded corners and avoid the sharp square corners of each line."""
-	ax.plot([L1l,L1r],[0,0],'k',linewidth=linewidth,solid_capstyle='round')
-	ax.plot([L1l,0],[0,L2],'k',linewidth=linewidth,solid_capstyle='round')
-	ax.plot([L1r,0],[0,L2],'k',linewidth=linewidth,solid_capstyle='round')
+    ax.plot([L1l,L1r],[0,0],'k',linewidth=linewidth,solid_capstyle='round')
+    ax.plot([L1l,0],[0,L2],'k',linewidth=linewidth,solid_capstyle='round')
+    ax.plot([L1r,0],[0,L2],'k',linewidth=linewidth,solid_capstyle='round')
 
-	#Equalize aspect ratio of x and y axes
-	ax.set_aspect('equal')
+    #Equalize aspect ratio of x and y axes
+    ax.set_aspect('equal')
 
 plt.show()
 
--- billiard_funcs.py
+++ billiard_funcs.py
@@ -1,61 +1,149 @@
 import numpy as np
 
+class Triangle:
+    """ Specifies triangle shape and other properties
+    Vertex coordinates are (L1r,0), (L1l,0), (0,L2)
+       /|\    |
+      / | \   L2
+     /__|__\  |
+     L1l L1R
+    """
+    def __init__(self,L1l,L1r,L2):
+        self.L1l = L1l
+        self.L1r = L1r
+        self.L2 = L2
+        self.left_side_vect = np.array([1,-L2/L1l]) / np.sqrt(1 + L2**2/L1l**2)
+        self.right_side_vect = np.array([1,-L2/L1r]) / np.sqrt(1 + L2**2/L1r**2)
 
-def trajectory_collisions(x,y,vx,vy,t,L1l,L1r,L2):
-	# returns only the points of collision, in addition to the initial and final point
 
-	right_side_vect = np.array([1,-L2/L1r]) / np.sqrt(1 + L2**2/L1r**2)
-	left_side_vect = np.array([1,-L2/L1l]) / np.sqrt(1 + L2**2/L1l**2)
+t_list = np.zeros(3) # Initializes array to be used in next_collision
+def next_collision(x,y,vx,vy,max_t,triangle):
+    """ Computes the state of particle immediate following the next collision
+        with a wall, UNLESS no collision occurs within time max_t, in which
+        case it returns that state of the particle after time max_t.
 
-	t_list = np.zeros(3)
-	def next_collision(x,y,vx,vy,t):
-		t_left = (L2 - L2*x/L1l - y) / (vy + L2*vx/L1l)
-		t_bottom = -y/vy
-		t_right = (L2 - L2*x/L1r - y) / (vy + L2*vx/L1r)
+        Parameters:
+            x: initial x of particle
+            y: initial y of particle
+            vx: initial vx of particle
+            vy: initial vy of particle
+            max_t: Maximum time
+            triangle: Triangle object that defines the walls
 
-		t_list[0],t_list[1],t_list[2] = t_left,t_bottom,t_right
-		t_list[t_list<=0] = np.inf
+        Returns:
+            x: final x after collision
+            y: final y after collision
+            vx: final vx after collision
+            vy: final vy after collision
+            collision_time: time elapsed until collision OR max_t if no 
+                            collision occurs.
+            collision_occured: TRUE if collision occurs, False otherwise.
+            
 
-		if np.all(t_list>t): # No collision
-			return x+vx*t, y+vy*t, vx, vy, None
-		else:
-			collision_wall = np.argmin(t_list)
-			collision_time = t_list[collision_wall]
-			if collision_wall==0: #left
-				x += vx*collision_time
-				y = L2*(1.-x/L1l) - 0.00000001
-				v_parallel = left_side_vect * (vx*left_side_vect[0] + vy*left_side_vect[1])
-				v_perpendicular = [vx - v_parallel[0], vy - v_parallel[1]]
-				vx = v_parallel[0] - v_perpendicular[0]
-				vy = v_parallel[1] - v_perpendicular[1]
-			elif collision_wall==1: #bottom
-				y = 0.
-				x += vx*collision_time
-				vy *= -1
-			else: #hypotenuse
-				x += vx*collision_time
-				y = L2*(1.-x/L1r) - 0.00000001
-				v_parallel = right_side_vect * (vx*right_side_vect[0] + vy*right_side_vect[1])
-				v_perpendicular = [vx - v_parallel[0], vy - v_parallel[1]]
-				vx = v_parallel[0] - v_perpendicular[0]
-				vy = v_parallel[1] - v_perpendicular[1]
-			return x,y,vx,vy,collision_time
-	
-	xs = [x]
-	ys = [y]
-	ts = [0]
-	for i in range(10000):
-		x,y,vx,vy,collision_time = next_collision(xs[-1],ys[-1],vx,vy,t-ts[-1])
-		xs += [x]
-		ys += [y]
-		if collision_time==None:
-			ts += [t]
-			break
-		else:
-			ts += [ts[-1]+collision_time]
-	else:
-		print('reached end of loop without finishing.')
-		quit()
+    """
+    t_left = (triangle.L2 - triangle.L2*x/triangle.L1l - y) / (vy + triangle.L2*vx/triangle.L1l)
+    t_bottom = -y/vy
+    t_right = (triangle.L2 - triangle.L2*x/triangle.L1r - y) / (vy + triangle.L2*vx/triangle.L1r)
 
-	return xs,ys,ts
+    t_list[0],t_list[1],t_list[2] = t_left,t_bottom,t_right
+    t_list[t_list<=0] = np.inf
 
+    if np.all(t_list>max_t): # No collision
+        return x+vx*max_t, y+vy*max_t, vx, vy, max_t, False
+    else:
+        collision_wall = np.argmin(t_list)
+        collision_time = t_list[collision_wall]
+        if collision_wall==0: #left
+            x += vx*collision_time
+            y = triangle.L2*(1.-x/triangle.L1l) - 0.00000001
+            v_parallel = triangle.left_side_vect * (vx*triangle.left_side_vect[0] + vy*triangle.left_side_vect[1])
+            v_perpendicular = [vx - v_parallel[0], vy - v_parallel[1]]
+            vx = v_parallel[0] - v_perpendicular[0]
+            vy = v_parallel[1] - v_perpendicular[1]
+        elif collision_wall==1: #bottom
+            y = 0.
+            x += vx*collision_time
+            vy *= -1
+        else: #hypotenuse
+            x += vx*collision_time
+            y = triangle.L2*(1.-x/triangle.L1r) - 0.00000001
+            v_parallel = triangle.right_side_vect * (vx*triangle.right_side_vect[0] + vy*triangle.right_side_vect[1])
+            v_perpendicular = [vx - v_parallel[0], vy - v_parallel[1]]
+            vx = v_parallel[0] - v_perpendicular[0]
+            vy = v_parallel[1] - v_perpendicular[1]
+        return x,y,vx,vy,collision_time, True
+
+
+def trajectory_collisions(x,y,vx,vy,t,triangle):
+    """ Generates a trajectory where the position and time of each collision
+        are recorded.
+
+        Parameters:
+            x: initial x of particle
+            y: initial y of particle
+            vx: initial vx of particle
+            vy: initial vy of particle
+            t: trajectory time
+            triangle: Triangle object that defines the walls
+
+        Returns:
+            xs: x components of trajectory. xs[0] = x (initial condition) and
+                xs[-1] is the particle position after time t, which is NOT
+                the time at which a collision occurs (in general).
+            ys: y components of trajectory. ys[0] = y (initial condition) and
+                ys[-1] is the particle position after time t, which is NOT
+                the time at which a collision occurs (in general).
+            ts: times of each collision and final point. ts[0] = 0.
+
+    """
+
+    xs = [x]
+    ys = [y]
+    ts = [0]
+    for i in range(10000):
+        x,y,vx,vy,collision_time, collision_occured = next_collision(xs[-1],ys[-1],vx,vy,t-ts[-1],triangle)
+        xs += [x]
+        ys += [y]
+        if collision_occured:
+            ts += [ts[-1]+collision_time]
+        else:
+            ts += [t] # Append final time
+            break
+    else:
+        print('reached end of loop without finishing.')
+        quit()
+
+    return xs,ys,ts
+
+
+def propogate(x,y,vx,vy,t,triangle):
+    """ Propogates a particle forward by time t
+
+        Parameters:
+            x: initial x of particle
+            y: initial y of particle
+            vx: initial vx of particle
+            vy: initial vy of particle
+            t: trajectory time
+            triangle: Triangle object that defines the walls
+
+        Returns:
+            x: final x
+            y: final y
+            vx: final vx     
+            vy: final vy     
+
+
+        This is a recursive function which steps forward collision-after-collision
+        using the next_collision function until the particle has been propogated
+        forward by time t.
+
+    """
+    x,y,vx,vy,collision_time,collision_occured  = next_collision(x,y,vx,vy,t,triangle)
+    if collision_occured:
+        return propogate(x,y,vx,vy,t-collision_time,triangle)
+    else:
+        return x,y,vx,vy
+
+
+#
